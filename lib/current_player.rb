b6ad5e689bfe2e9948b8c4bebb750ddcecf0ff77
Max Ringelheim <max.ringelheim@gmail.com>
2020-02-07 14:09:14 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -3,7 +3,7 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 def turn_count(board)
   counter = 0
-  turn_count(board).each do |count|
+  board.each do |count|
     puts "number of turns played is #{count}"
   counter += 1
   end
