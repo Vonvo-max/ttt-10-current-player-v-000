de10aef111f3d165c723bf3763a7007971fc078c
Max Ringelheim <max.ringelheim@gmail.com>
2020-02-07 15:12:19 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -3,7 +3,7 @@
 def turn_count(board)
   counter = 0
   board.each do |count|
-    if board == "X" || board == "O"
+    if board == "X" || board == "O" || board == " "
       counter += 1
    end
   end
