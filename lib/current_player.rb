92fd54f07fb802aa1ffa057554abbb0adf03191a
Max Ringelheim <max.ringelheim@gmail.com>
2020-02-07 14:25:13 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -4,15 +4,16 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def turn_count(board)
   counter = 0
   board.each do |count|
+    if board == "X" || board == "O"
+      counter += 1
     puts "number of turns played is #{count}"
-  counter += 1
   end
 end
 
 
 
 def current_player(board)
-  if board % 2 == 0
+  if turn_count(board) % 2 == 0
     return "X"
   else return "O"
   end
